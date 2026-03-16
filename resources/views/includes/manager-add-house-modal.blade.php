1073516921bad1f1b1097a48c5a0a7d5846091c4
Clive-04 <clivebenito21@gmail.com>
2026-03-16 18:45:32 +0800

    refactor: modified the manager side for add and edit to match the UI

--- resources/views/includes/manager-add-house-modal.blade.php
+++ resources/views/includes/manager-add-house-modal.blade.php
@@ -6,32 +6,21 @@
         </div>
 
         <form id="addHouseForm">
-            <div class="modal-group">
-                <label for="houseName">House Name</label>
-                <input type="text" id="houseName" placeholder="Enter house name" required>
-            </div>
+            <div class="modal-row">
+                <div class="modal-group">
+                    <label for="houseName">House Number</label>
+                    <input type="text" id="houseName" required>
+                </div>
 
-            <div class="modal-group">
-                <label for="houseStatusInput">Status</label>
-                <select id="houseStatusInput">
-                    <option value="Active">Active</option>
-                    <option value="Inactive">Inactive</option>
-                </select>
-            </div>
-
-            <div class="modal-group">
-                <label for="houseBatchInput">Batch</label>
-                <input type="text" id="houseBatchInput" placeholder="Enter batch">
-            </div>
-
-            <div class="modal-group">
-                <label for="housePenCount">Number of Pens</label>
-                <input type="number" id="housePenCount" min="1" value="1">
+                <div class="modal-group">
+                    <label for="housePenCount">Number of Pens</label>
+                    <input type="number" id="housePenCount" min="1" value="1">
+                </div>
             </div>
 
             <div class="modal-actions">
                 <button type="button" class="cancel-btn" id="cancelAddHouseModal">Cancel</button>
-                <button type="submit" class="save-btn">Save House</button>
+                <button type="submit" class="save-btn">Save</button>
             </div>
         </form>
     </div>
